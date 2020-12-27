5731376c60dc29db7442f52cc1f32873a19355f5
y1zzy <wuy20@rpi.edu>
2020-12-27 16:35:36 -0500

    laptop set up

--- Quarantine/Assets/Scripts/actions.cs
+++ Quarantine/Assets/Scripts/actions.cs
@@ -6,24 +6,22 @@ using UnityEngine.UI;
 public class actions : MonoBehaviour
 {
     public string whatISee;
-    public Animator doorAnim;
-    public Animator bedroomDoorAnim;
+    public Animator doorAnim, bedroomDoorAnim;
     public raycasting raycastobj;
-    private bool doorIsOpen;
-    private bool door2IsOpen;
+    private bool doorIsOpen, door2IsOpen;
     public bool onBed;
     private string interactableObj;
     public GameObject laptopScene;
     public player onBedStatus;
     public GameObject phoneScreen;
-    public GameObject wechatOn, wechatOff, discordOn, discordOff, plannerOn, plannerOff;
+    public device chat;
     public bool wechatting,discording,planner;
     public GameObject timeManager;
+    public laptop laptop;
     public lightswitch bedroomLight, bathroomLight, livingroomLight, kitchenLight;
     public bool bedroomSwitchOn, bathroomSwitchOn, livingroomSwitchOn, kitchenSwitchOn;
     void start()
     {
-     
         onBed = false;
         wechatting = false;
         discording = false;
@@ -33,35 +31,38 @@ public class actions : MonoBehaviour
     
     void Update()
     {
-        whatISee = raycastobj.InteractText; //access the object info from raycasting\
+        whatISee = raycastobj.InteractText; //access the object info from raycasting
         
        // Debug.Log(whatISee);
         if (whatISee == "door")
         {
             if (Input.GetKeyDown(KeyCode.E))
-            {
-                openTheDoor();
-            }  
+            {openTheDoor();}  
         }
         if (whatISee == "bdoor")
         {
             if (Input.GetKeyDown(KeyCode.E))
-            {
-                open2Door();
-            }
+            {open2Door();}
         }
 
-        if (whatISee == "Laptop")
+        if (whatISee == "msi")
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
-                laptoptask();
+                if (laptop.laptopOpen == false)
+                {
+                    laptop.laptopOpen = true;
+                }
+            }
+            if (laptop.laptopOpen == true && Input.GetKeyDown(KeyCode.Escape))
+            {
+                laptop.laptopOpen = false;
             }
         }
+        
 
         if (whatISee == "bedroomswitch")
         {
-            Debug.Log(bedroomLight.isOn);
             if (Input.GetKeyDown(KeyCode.T))
             {
                 if (bedroomLight.isOn == true)
@@ -120,6 +121,22 @@ public class actions : MonoBehaviour
             }
         }
 
+        
+
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (chat.chatIsOn == true)
+            {
+                chat.chatIsOn = false;
+            }
+            else
+            {
+                chat.chatIsOn = true;
+            }
+        }
+        
+
 
         if (whatISee == "bed")
         {
@@ -139,39 +156,7 @@ public class actions : MonoBehaviour
             }
         }
 
-        /*if (Input.GetKeyDown(KeyCode.Alpha1))//this condition opens up the wechat panel
-        {
-            discordOn.SetActive(false);
-            discordOff.SetActive(true);
-            discording = false;
-            plannerOn.SetActive(false);
-            plannerOff.SetActive(true);
-            planner = false;
-            wechat();
-            
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha2))//this condition opens up the wechat panel
-        {
-            wechatOn.SetActive(false);
-            wechatOff.SetActive(true);
-            wechatting = false;
-            plannerOn.SetActive(false);
-            plannerOff.SetActive(true);
-            planner = false;
-            discord();
-        }
-
-        if (Input.GetKeyDown(KeyCode.Alpha3))//this condition opens up the wechat panel
-        {
-            wechatOn.SetActive(false);
-            wechatOff.SetActive(true);
-            wechatting = false;
-            discordOn.SetActive(false);
-            discordOff.SetActive(true);
-            discording = false;
-            openplanner();
-        }*/
+       
     }
 
     void openTheDoor() // this function is to open the bathroom door
@@ -225,56 +210,4 @@ public class actions : MonoBehaviour
 
     }
     
-
-    void wechat()
-    {
-        if (!wechatting)
-        {
-            
-            wechatOn.SetActive(true);
-            wechatOff.SetActive(false);
-            wechatting = true;
-
-        }
-        else
-        {
-            wechatOn.SetActive(false);
-            wechatOff.SetActive(true);
-            wechatting = false;
-        }
-    }
-    void discord()
-    {
-        if (!discording)
-        {
-
-            discordOn.SetActive(true);
-            discordOff.SetActive(false);
-            discording = true;
-
-        }
-        else
-        {
-            discordOn.SetActive(false);
-            discordOff.SetActive(true);
-            discording = false;
-        }
-    }
-    void openplanner()
-    {
-        if (!planner)
-        {
-
-            plannerOn.SetActive(true);
-            plannerOff.SetActive(false);
-            planner = true;
-
-        }
-        else
-        {
-            plannerOn.SetActive(false);
-            plannerOff.SetActive(true);
-            planner = false;
-        }
-    }
 }
---
+++ Quarantine/Assets/Scripts/laptop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laptop : MonoBehaviour
+{
+    public GameObject screen;
+    public bool laptopOpen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        laptopOpen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (laptopOpen == false)
+        {
+            screen.SetActive(false);
+        }
+        if (laptopOpen == true)
+        {
+            screen.SetActive(true);
+        }
+    }
+}
