0fc6dbf866205a8849e46605a53a18d4f0e43e3b
y1zzy <wuy20@rpi.edu>
2021-01-22 00:47:59 -0500

    trying to do the message system

---
+++ Quarantine/Assets/Scripts/dialogue.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class dialogue : MonoBehaviour
+{
+
+    public GameObject chatPanel, textObject;
+
+    [SerializeField]
+    List<Message> messageList = new List<Message>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("pressed");
+        }
+    }
+
+    public void chat()
+        {
+        }
+}
+[System.Serializable]
+public class Message
+{
+    public string text;
+    public Text textobject;
+}
+
